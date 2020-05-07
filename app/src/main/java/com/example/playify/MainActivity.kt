66ef781ab1d1f8d6863e730002f9c5d753b7e0a4
Josh Heidecker <heidecjj@gmail.com>
2020-05-07 01:01:59 -0400

    Added instructions for preventing spotify from opening spotify links
    closes #2

--- app/src/main/java/com/example/playify/MainActivity.kt
+++ app/src/main/java/com/example/playify/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.playify
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.view.View
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        var pathText = findViewById<TextView>(R.id.spotifySettingsPath)
+
+        var ver = Build.VERSION.SDK_INT
+        when {
+            ver >= Build.VERSION_CODES.P ->
+                pathText.text = getString(R.string.spotify_settings_path_v28)
+            ver >= Build.VERSION_CODES.M ->
+                pathText.text = getString(R.string.spotify_settings_path_v23)
+            else -> pathText.text = getString(R.string.spotify_settings_path_v9)
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        showHideSpotifyInstructions()
+    }
+
+    private fun showHideSpotifyInstructions() {
+        var spotifyCard = findViewById<CardView>(R.id.spotifyInstructionsCard)
+        try {
+            packageManager.getPackageInfo("com.spotify.music", 0)
+            spotifyCard.visibility = View.VISIBLE
+        } catch (err: PackageManager.NameNotFoundException) {
+            spotifyCard.visibility = View.GONE
+        }
+    }
+
+    fun startSpotifyAppInfo(view: View) {
+        startActivity(Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
+            .setData(Uri.parse("package:com.spotify.music")))
+    }
+
 }
